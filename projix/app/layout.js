9a132480b542d2f1719afaf932f1bd170d2e0758
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-23 11:48:22 +0530

    basic Styles

--- projix/app/layout.js
+++ projix/app/layout.js
@@ -1,4 +1,9 @@
+import Header from "@/components/header";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Create Next App",
@@ -8,8 +13,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className={`${inter.className}`}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+         <Header />
+          <main className="min-h-screen">
+            {children}
+          </main>
+          <footer className="bg-gray-900 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p>Made with 💗 by Sankeerthan</p>
+            </div>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
   );
