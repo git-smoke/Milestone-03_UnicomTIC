8ff223d6801baa321f280be648ba46885db09a27
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-25 01:07:49 +0530

    setupConnectiontoDB

--- projix/components/header.jsx
+++ projix/components/header.jsx
@@ -1,25 +1,41 @@
-
-import { SignInButton, UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+import UserMenu from "./user-menu";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src="/logo2.png"
+            alt="projix Logo"
+            width={200}
+            height={56}
+            classname="h-10 w-auto object-contain"
+          />
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link href="/project/create">
+            <Button variant="destructive" classname="flex items-center gap-2">
+              <PenBox size={18} />
+              <span>Create Project</span>
+            </Button>
+          </Link>
 
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding" />
+            <Button variant="outline">Login</Button>
+          </SignedOut>
+
+          <SignedIn>
+            <UserMenu />
+          </SignedIn>
+        </div>
       </nav>
- 
- 
- 
- 
- 
- 
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
     </header>
   );
 };
