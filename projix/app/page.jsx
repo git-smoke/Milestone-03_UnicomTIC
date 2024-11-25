e207874c0b918c5a9e7c1bb4dfb6ae6810513b15
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-25 12:34:38 +0530

    added hero image

--- projix/app/page.jsx
+++ projix/app/page.jsx
@@ -5,7 +5,20 @@ export default function Home() {
     <div>
       {/* Hero Section*/}
       <section className="">
-        <h1>Streamline Your Workflow</h1>
+        <h1>
+          Streamline Your Workflow
+          <br />
+          <span>
+            with{" "}
+            <Image
+              src={"/logo2.png"}
+              alt="projix Logo"
+              width={400}
+              height={80}
+              className="h-14 sm:h-24 w-auto object-contain"
+            />
+          </span>
+        </h1>
       </section>
     </div>
   );
