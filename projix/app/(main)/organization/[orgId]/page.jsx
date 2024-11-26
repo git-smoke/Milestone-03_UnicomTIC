8ce466c3645e3fd2e718732433c49944a307672d
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-26 23:24:31 +0530

    added org switcher comp

--- projix/app/(main)/organization/[orgId]/page.jsx
+++ projix/app/(main)/organization/[orgId]/page.jsx
@@ -1,4 +1,5 @@
 import { getOrganization } from "@/actions/organization";
+import OrgSwitcher from "@/components/org-switcher";
 import React from "react";
 
 const Organization = async ({ params }) => {
@@ -13,9 +14,11 @@ const Organization = async ({ params }) => {
   return (
     <div className="container mx-auto">
       <div className="mb-4 flex flex-col sm:flex-row justify-between items-start">
-        <h1 className="text-5xl font-bold gradient-title pb-2">{organization.name}'s Project </h1>
+        <h1 className="text-5xl font-bold gradient-title pb-2">
+          {organization.name}'s Project{" "}
+        </h1>
 
-        {/* org switcher */}
+        <OrgSwitcher />
       </div>
       <div className="mb-4">Show org projects</div>
       <div className="mt-8">Show user assigned and reported issues here</div>
