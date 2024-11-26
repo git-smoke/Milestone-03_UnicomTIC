cdd303f30b945b31a812eb816b0195f53019310b
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-26 21:28:38 +0530

    changes made for onboarding

--- projix/middleware.js
+++ projix/middleware.js
@@ -1,5 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/onboarding(.*)",
@@ -8,19 +8,25 @@ const isProtectedRoute = createRouteMatcher([
   "/issue(.*)",
 ]);
 
-export default clerkMiddleware(
-  (auth, req) => {
-    if(!auth().userId && isProtectedRoute(req)){
-      return auth().redirectToSignIn();
-    }
+export default clerkMiddleware((auth, req) => {
+  if (!auth().userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn();
   }
-);
+
+  if (
+    auth().userId &&
+    !auth().userId &&
+    (req.nextUrl.pathname !== "/onboarding") & (req.nextUrl.pathname !== "/")
+  ) {
+    return NextResponse.redirect(new URL("/onboarding", req.url));
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
