8f147da674d028ba64f9c6a8a241365f17f19a1f
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-26 23:15:32 +0530

    styles for org page

--- projix/app/(main)/organization/[orgId]/page.jsx
+++ projix/app/(main)/organization/[orgId]/page.jsx
@@ -1,8 +1,26 @@
+import { getOrganization } from "@/actions/organization";
 import React from "react";
 
-const Organization = ({ params }) => {
+const Organization = async ({ params }) => {
   const { orgId } = params;
-  return <div>{orgId}</div>;
+
+  const organization = await getOrganization(orgId);
+
+  if (!organization) {
+    return <div>Organization Not Found</div>;
+  }
+
+  return (
+    <div className="container mx-auto">
+      <div className="mb-4 flex flex-col sm:flex-row justify-between items-start">
+        <h1 className="text-5xl font-bold gradient-title pb-2">{organization.name}'s Project </h1>
+
+        {/* org switcher */}
+      </div>
+      <div className="mb-4">Show org projects</div>
+      <div className="mt-8">Show user assigned and reported issues here</div>
+    </div>
+  );
 };
 
 export default Organization;
