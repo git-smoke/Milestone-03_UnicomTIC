ac9aeccb1a7c137cdcd70f9234d40f9795d93fba
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-27 11:20:08 +0530

    member credintial bug fixed

--- projix/app/(main)/project/create/page.jsx
+++ projix/app/(main)/project/create/page.jsx
@@ -1,6 +1,45 @@
 "use client";
+
+import OrgSwitcher from "@/components/org-switcher";
+import { useOrganization, useUser } from "@clerk/nextjs";
+
+import { useEffect, useState } from "react";
+
 const CreateProjectPage = () => {
-  return <div>CreateProjectPage</div>;
+  const { isLoaded: isOrgLoaded, membership } = useOrganization();
+  const { isLoaded: isUserLoaded } = useUser();
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    if (isOrgLoaded && isUserLoaded && membership) {
+      setIsAdmin(membership.role === "org:admin");
+    }
+  }, [isOrgLoaded, isUserLoaded, membership]);
+
+  if (!isOrgLoaded || !isUserLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAdmin) {
+    return (
+      <div className="flex flex-col gap-2 items-center">
+        <span className="text-2xl gradient-title">
+          Oops! Only Admins can create projects.
+        </span>
+        <OrgSwitcher />
+      </div>
+    );
+  }
+
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-6xl text-center font-bold mb-8 gradient-title">
+        Create New Project
+      </h1>
+
+      <form action=""></form>
+    </div>
+  );
 };
 
 export default CreateProjectPage;
