730931ec8179720aa971b9cc1f0b99941b764aea
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-27 22:58:26 +0530

    project listing ui updated + bugs fixed

--- projix/app/(main)/organization/[orgId]/_components/project-list.jsx
+++ projix/app/(main)/organization/[orgId]/_components/project-list.jsx
@@ -1,15 +1,14 @@
-""
-
 import { getProjects } from "@/actions/projects";
-import { Link } from "next/link";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default async function ProjectList({ orgId }) {
   const projects = await getProjects(orgId);
 
-  if (projects.length !== 0) {
+  if (projects.length === 0) {
     return (
       <p>
-        No Projects Found.{""}
+        No Projects Found.{" "}
         <Link
           href="/project/create"
           className="underline underline-offset-2 text-blue-200"
@@ -19,4 +18,29 @@ export default async function ProjectList({ orgId }) {
       </p>
     );
   }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      {projects.map((project) => {
+        return (
+          <Card key={project.id}>
+            <CardHeader>
+              <CardTitle className="flex justify-between items-center">{project.name}
+                <DeleteProject projectId={project.id} />
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-sm text-gray-500 mb-4">{project.description}</p>
+              <Link
+                href={`/project/${project.id}`}
+                className="text-blue-500 hover:underline"
+              >
+                View Project
+              </Link>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
