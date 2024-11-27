69231e6d4341d1df6223f74e35a25b154c9e0c40
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-27 19:43:59 +0530

    wrote component for project-list

---
+++ projix/app/(main)/organization/[orgId]/_components/project-list.jsx
@@ -0,0 +1,19 @@
+import { getProject } from "@/actions/projects";
+import { Link } from "next/link";
+export default async function ProjectList({ orgId }) {
+  const projects = await getProject(orgId);
+
+  if (projects.length === 0) {
+    return (
+      <p>
+        No Projects Found.{""}
+        <Link
+          href="/project/create"
+          className="underline underline-offset-2 text-blue-200"
+        >
+          Create New
+        </Link>
+      </p>
+    );
+  }
+}
