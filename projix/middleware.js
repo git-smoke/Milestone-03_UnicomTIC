23269fe9b7d4d3eb3efed253ec5a0009adb2ba98
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-27 07:58:11 +0530

    bug fixing in middleware

--- projix/middleware.js
+++ projix/middleware.js
@@ -16,7 +16,7 @@ export default clerkMiddleware((auth, req) => {
   if (
     auth().userId &&
     !auth().userId &&
-    (req.nextUrl.pathname !== "/onboarding") & (req.nextUrl.pathname !== "/")
+    (req.nextUrl.pathname !== "/onboarding") && (req.nextUrl.pathname !== "/")
   ) {
     return NextResponse.redirect(new URL("/onboarding", req.url));
   }
