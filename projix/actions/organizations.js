7084bd15d09ea26246631593cf5de1d7154ecce9
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-27 22:10:01 +0530

    name conflict solved

---
+++ projix/actions/organizations.js
@@ -0,0 +1,46 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+
+export async function getOrganization(slug) {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const organization = await clerkClient().organizations.getOrganization({
+    slug,
+  });
+
+  if (!organization) {
+    return null;
+  }
+
+  const { data: membership } =
+    await clerkClient().organizations.getOrganizationMembershipList({
+      organizationId: organization.id,
+    });
+
+  const userMembership = membership.find(
+    (member) => member.publicUserData.userId === userId
+  );
+
+  //if user is not a member, return null
+  if (!membership) {
+    return null;
+  }
+
+  return organization;
+}
