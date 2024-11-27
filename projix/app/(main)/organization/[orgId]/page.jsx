683ae3d7350c7eda9fe25943ae307f109bb159fa
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-27 08:41:14 +0530

    bug fixed to authorization

--- projix/app/(main)/organization/[orgId]/page.jsx
+++ projix/app/(main)/organization/[orgId]/page.jsx
@@ -15,7 +15,7 @@ const Organization = async ({ params }) => {
     <div className="container mx-auto">
       <div className="mb-4 flex flex-col sm:flex-row justify-between items-start">
         <h1 className="text-5xl font-bold gradient-title pb-2">
-          {organization.name}'s Project{" "}
+          {organization.name}&rsquo; s Project{" "}
         </h1>
 
         <OrgSwitcher />
--- projix/app/(main)/project/create/page.jsx
+++ projix/app/(main)/project/create/page.jsx
@@ -1,34 +1,9 @@
-"use client";
-
-import OrgSwitcher from "@/components/org-switcher";
-import { useOrganization, useUser } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
+import React from 'react'
 
 const CreateProjectPage = () => {
-  const { isLoaded: isOrgLoaded, membership } = useOrganization();
-  const { isLoaded: isUserLoaded } = useUser();
-  const [isAdmin, setIsAdmin] = useState(false);
+  return (
+    <div>CreateProjectPage</div>
+  )
+}
 
-  useEffect(() => {
-    if (isOrgLoaded && isUserLoaded && membership) {
-      setIsAdmin(membership.role === "org:admin");
-    }
-  }, [isOrgLoaded, isUserLoaded, membership]);
-
-  if (!isOrgLoaded || isUserLoaded) {
-    return null;
-  }
-
-  if (!isAdmin) {
-    return (
-      <div>
-        <span>Oops! Only Admins can create projects.</span>
-        <OrgSwitcher />
-      </div>
-    );
-  }
-
-  return <div>CreateProjectPage</div>;
-};
-
-export default CreateProjectPage;
+export default CreateProjectPage
