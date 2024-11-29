1a420dff129434e2bed6aba2f7a19204b38edd40
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-29 21:42:11 +0530

    added some logics in sprint board component

---
+++ projix/app/(main)/project/_components/sprint-board.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const SprintBoard = ({ sprints, pojectId, orgId }) => {
+  const [currentSprint, setCurrentSprint] = useState(
+    sprints.find((spr) => spr.status === "ACTIVE") || sprints[0]
+  );
+
+  return (
+    <div>
+      {/* Sprint Manager */}
+      <SprintManager
+        sprint={currentSprint}
+        setSprint={setCurrentSprint}
+        sprints={sprints}
+        projectId={projectId}
+      />
+
+      {/* Kandban Board */}
+    </div>
+  );
+};
+
+export default SprintBoard;
