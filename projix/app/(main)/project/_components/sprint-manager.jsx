1bdbcee5a7fc26ee849fe2025ed34b3889214c99
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-29 22:59:13 +0530

    created and written logics for sprint manager

--- projix/app/(main)/project/_components/sprint-manager.jsx
+++ projix/app/(main)/project/_components/sprint-manager.jsx
@@ -1,7 +1,57 @@
 "use client";
 
+import {
+  Select,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+import { format, isAfter, isBefore } from "date-fns";
+import { useState } from "react";
+
 const SprintManager = ({ sprint, setSprint, sprints, projectId }) => {
-  return <div>SprintManager</div>;
+  const [status, setStatus] = useState(sprint.status);
+
+  const startDate = new Date(sprint.startDate);
+  const endDate = new Date(sprint.endDate);
+  const now = new Date();
+
+  const canStart =
+    isBefore(now, endDate) && isAfter(now, startDate) && status === "PLANNED";
+
+  const canEnd = status === "ACTIVE";
+
+  const handleSprintChange = () => {
+    const selectedSprint = sprints.find((s) => s.id === value);
+    setSprint(selectedSprint);
+    setStatus(selectedSprint.status);
+  };
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <Select value={sprint.id} onValueChange={handleSprintChange}>
+          <SelectTrigger className="bg-slate-950 self-start">
+            <SelectValue placeholder="Select Sprint" />
+          </SelectTrigger>
+          <SelectContent>
+            {sprints.map((sprint) => {
+              return (
+                <SelectItem key={sprint.id} value={sprint.id}>
+                  {sprint.name} ({format(sprint.startDate, "MMM d, yyyy")}) to{" "}
+                  {format(sprint.endDate, "MMM d, yyyy")}
+                </SelectItem>
+              );
+            })}
+          </SelectContent>
+        </Select>
+
+        {canStart && (
+          <Button className="bg-green-900 text-white">Start Sprint</Button>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default SprintManager;
