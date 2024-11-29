5e708079e179dad7f73276032dcf9d503bcd3ead
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-29 21:22:48 +0530

    updated daypicker and ui

--- projix/app/(main)/project/_components/create-sprint.jsx
+++ projix/app/(main)/project/_components/create-sprint.jsx
@@ -3,10 +3,19 @@
 import { createSprint } from "@/actions/sprints";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { addDays } from "date-fns";
+import { addDays, format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { DayPicker } from "react-day-picker";
+import { Controller, useForm } from "react-hook-form";
+import "react-day-picker/dist/style.css";
 
 const SprintCreationForm = ({
   projectTitle,
@@ -21,7 +30,12 @@ const SprintCreationForm = ({
     to: addDays(new Date(), 14),
   });
 
-  useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm({
     resolver: zodResolver(createSprint),
     defaultValues: {
       name: `${projectKey}-${sprintKey}`,
@@ -32,8 +46,10 @@ const SprintCreationForm = ({
 
   return (
     <>
-      <div>
-        <h1>{projectTitle}</h1>
+      <div className="flex justify-between">
+        <h1 className="text-5xl font-bold mb-8 gradient-title">
+          {projectTitle}
+        </h1>
         <Button
           className="mt-2"
           onClick={() => setShowForm(!showForm)}
@@ -43,11 +59,90 @@ const SprintCreationForm = ({
         </Button>
       </div>
 
-      {showForm && <Card>
-            <CardContent>
-                <form action=""></form>
-            </CardContent>
-        </Card>}
+      {showForm && (
+        <Card className="pt-4 mb-4">
+          <CardContent>
+            <form action="" className="flex gap-4 items-end">
+              <div className="flex-1">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium mb-1"
+                >
+                  Sprint Name
+                </label>
+                <Input
+                  id="name"
+                  readOnly
+                  className="bg-slate-950"
+                  {...register("name")}
+                />
+
+                {errors.name && (
+                  <p className="text-red-500 text-sm mt-1">{errors.message}</p>
+                )}
+              </div>
+              <div>
+                <label for="" className="block text-sm font-medium mb-1">
+                  Sprint Duration
+                </label>
+
+                <Controller
+                  control={control}
+                  name="dateRange"
+                  render={({ field }) => {
+                    return (
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <Button
+                            variant="outline"
+                            className={`w-full justify-start text-left font-normal bg-slate-950 ${
+                              !dateRange && "text-muted-foreground"
+                            }`}
+                          >
+                            <CalendarIcon className="mr-2 h-4 w-4" />
+                            {dateRange.from && dateRange.to ? (
+                              format(dateRange.from, "LLL dd, y") +
+                              " - " +
+                              format(dateRange.to, "LLL dd, y")
+                            ) : (
+                              <span>Pick a date</span>
+                            )}
+                          </Button>
+                        </PopoverTrigger>
+                        <PopoverContent
+                          className="
+                        w-auto bg-slate-900
+                        "
+                          align="start"
+                        >
+                          <DayPicker
+                            mode="range"
+                            selected={dateRange}
+                            onSelect={(range) => {
+                              if (range?.from && range?.to) {
+                                setDateRange(range);
+                                field.onChange(range);
+                              }
+                            }}
+                            classNames={{
+                              chevron: "fill-blue-500",
+                              range_start: "bg-blue-700",
+                              range_end: "bg-blue-700",
+                              range_middle: "bg-blue-400",
+                              day_button: "border-none",
+                              today: "border-2 border-blue-700",
+                            }}
+                          />
+                        </PopoverContent>
+                      </Popover>
+                    );
+                  }}
+                />
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      )}
     </>
   );
 };
