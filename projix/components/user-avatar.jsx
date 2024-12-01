f895ac45931c3d1459e579096dfddf1498cffc9f
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-02 03:02:49 +0530

    adjusted Avatar height and width

--- projix/components/user-avatar.jsx
+++ projix/components/user-avatar.jsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 const UserAvatar = () => {
   return (
     <div className="flex items-center space-x-2 w-full">
-      <Avatar>
+      <Avatar className="h-6 w-6">
         <AvatarImage src={user?.imageUrl} alt={user?.name} />
         <AvatarFallback className="capitalize">
           {user ? user.name : "?"}
