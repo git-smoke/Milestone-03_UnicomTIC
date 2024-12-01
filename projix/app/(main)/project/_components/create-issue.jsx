8bf5c3237f0e2ee42773dece06be583884adf080
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-01 15:18:34 +0530

    added params for issue creation drawer fn

--- projix/app/(main)/project/_components/create-issue.jsx
+++ projix/app/(main)/project/_components/create-issue.jsx
@@ -1,3 +1,17 @@
 "use client";
 
-const IssueCreationDrawer = () => {};
+import { Drawer } from "@/components/ui/drawer";
+
+const IssueCreationDrawer = ({
+  isOpen,
+  onClose,
+  sprintId,
+  status,
+  projectId,
+  onIssueCreated,
+  orgId,
+}) => {
+  return <Drawer open={isOpen} onClose={onClose}></Drawer>;
+};
+
+export default IssueCreationDrawer;
