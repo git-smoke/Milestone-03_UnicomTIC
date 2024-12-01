3a580adb8863c3bc9fcc2963011c15d5fcc7eb57
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-02 03:01:05 +0530

    updated content for user avatar component

--- projix/components/user-avatar.jsx
+++ projix/components/user-avatar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Avatar } from "./ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const UserAvatar = () => {
   return (
     <div className="flex items-center space-x-2 w-full">
-      <Avatar></Avatar>
+      <Avatar>
+        <AvatarImage src={user?.imageUrl} alt={user?.name} />
+        <AvatarFallback className="capitalize">
+          {user ? user.name : "?"}
+        </AvatarFallback>
+      </Avatar>
+      <span className="text-xs text-gray-500">
+        {user ? user.name : "Unassigned"}
+      </span>
     </div>
   );
 };
