e6a3a1876bf6abe48a5bac824ca3f3127d0291db
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-02 02:50:07 +0530

    updated issues card ui

--- projix/components/issue-cards.jsx
+++ projix/components/issue-cards.jsx
@@ -1,3 +1,6 @@
+import { Badge } from "./ui/badge";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
 const priorityColor = {
   LOW: "border-green-600",
   MEDIUM: "border-yellow-300",
@@ -5,8 +8,27 @@ const priorityColor = {
   URGENT: "border-red-400",
 };
 
-const IssueCard = () => {
-  return <div>IssueCard</div>;
+const IssueCard = ({
+  issue,
+  showStatus = false,
+  onDelete = () => {},
+  onUpdate = () => {},
+}) => {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>{issue.title}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {showStatus && <Badge>{issue.status}</Badge>}
+          <Badge variant="outline" className="-ml-1">
+            {issue.priority}
+          </Badge>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default IssueCard;
