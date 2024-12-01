a1d40fb6246bbedf3b3b68ec6dc358a94e017070
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-01 16:02:27 +0530

    wrote functions for fetching issues by orgId

--- projix/app/(main)/project/_components/create-issue.jsx
+++ projix/app/(main)/project/_components/create-issue.jsx
@@ -1,6 +1,20 @@
 "use client";
 
-import { Drawer } from "@/components/ui/drawer";
+import { createIssue } from "@/actions/issues";
+import { getOrganizationUsers } from "@/actions/organizations";
+import { issueSchema } from "@/app/lib/validators";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import useFetch from "@/hooks/use-fetch";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Plus } from "lucide-react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { BarLoader } from "react-spinners";
 
 const IssueCreationDrawer = ({
   isOpen,
@@ -11,7 +25,45 @@ const IssueCreationDrawer = ({
   onIssueCreated,
   orgId,
 }) => {
-  return <Drawer open={isOpen} onClose={onClose}></Drawer>;
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(issueSchema),
+  });
+
+  const {
+    loading: createIssueLoading,
+    fn: createIssueFn,
+    error,
+    data: newIssue,
+  } = useFetch(createIssue);
+  const {
+    loading: usersLoading,
+    fn: fetchUsers,
+    data: users,
+  } = useFetch(getOrganizationUsers);
+
+  useEffect(() => {
+    if (isOpen && orgId) {
+      fetchUsers(orgId);
+    }
+  }, [isOpen, orgId]);
+
+  const onSubmit = async (data) => {};
+
+  return (
+    <Drawer open={isOpen} onClose={onClose}>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Create New Issue</DrawerTitle>
+        </DrawerHeader>
+        {usersLoading && <BarLoader width={"100%"} color="#36d7b7" />}
+      </DrawerContent>
+    </Drawer>
+  );
 };
 
 export default IssueCreationDrawer;
