b6af44176ea6d9fd503b993ce82703ef2b834c8a
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-01 17:48:12 +0530

    added description and priority

--- projix/app/(main)/project/_components/create-issue.jsx
+++ projix/app/(main)/project/_components/create-issue.jsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { createIssue } from "@/actions/issues";
-import { getOrganizationUsers }   from "@/actions/organizations";
+import { getOrganizationUsers } from "@/actions/organizations";
 import { issueSchema } from "@/app/lib/validators";
+import { Button } from "@/components/ui/button";
 import {
   Drawer,
   DrawerContent,
@@ -19,6 +20,7 @@ import {
 } from "@/components/ui/select";
 import useFetch from "@/hooks/use-fetch";
 import { zodResolver } from "@hookform/resolvers/zod";
+import MDEditor from "@uiw/react-md-editor";
 import { Plus } from "lucide-react";
 import { useEffect } from "react";
 import { Controller, useForm } from "react-hook-form";
@@ -53,6 +55,11 @@ const IssueCreationDrawer = ({
     error,
     data: newIssue,
   } = useFetch(createIssue);
+
+  useEffect( () => {
+
+  },[]);
+
   const {
     loading: usersLoading,
     fn: fetchUsers,
@@ -65,7 +72,13 @@ const IssueCreationDrawer = ({
     }
   }, [isOpen, orgId]);
 
-  const onSubmit = async (data) => {};
+  const onSubmit = async (data) => {
+    await createIssueFn(projectId, {
+      ...data,
+      status,
+      sprintId,
+    });
+  };
 
   return (
     <Drawer open={isOpen} onClose={onClose}>
@@ -74,36 +87,103 @@ const IssueCreationDrawer = ({
           <DrawerTitle>Create New Issue</DrawerTitle>
         </DrawerHeader>
         {usersLoading && <BarLoader width={"100%"} color="#36d7b7" />}
-        <form className="p-12 space-y-4">
-          <label htmlFor="title" className="block text-sm font-medium mb-1">
-            Title
-          </label>
-          <Input id="title" {...register("title")} />
-          {errors.title && (
-            <p className="text-red-500 text-sm mt-2">{errors.title.message}</p>
-          )}
-          <label for="assigneeId" className="block text-sm font-medium mb-1">
-            Assignee
-          </label>
-
-          <Controller
-            name="assigneeId"
-            control={control}
-            render={({ field }) => (
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select assignee" />
-                </SelectTrigger>
-                <SelectContent>
-                  {users?.map((user) => (
-                    <SelectItem key={user.id} value={user.id}>
-                      {user?.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+        <form className="p-12 space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="title" className="block text-sm font-medium mb-1">
+              Title
+            </label>
+            <Input id="title" {...register("title")} />
+            {errors.title && (
+              <p className="text-red-500 text-sm mt-2">
+                {errors.title.message}
+              </p>
             )}
-          />
+          </div>
+          <div>
+            <label for="assigneeId" className="block text-sm font-medium mb-1">
+              Assignee
+            </label>
+
+            <Controller
+              name="assigneeId"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select assignee" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {users?.map((user) => (
+                      <SelectItem key={user.id} value={user.id}>
+                        {user?.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              )}
+            />
+            {errors.assigneeId && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.assigneeId.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label for="description" className="block text-sm font-medium mb-1">
+              Description
+            </label>
+            <Controller
+              name="assigneeId"
+              control={control}
+              render={({ field }) => (
+                <MDEditor value={field.value} onChange={field.onChange} />
+              )}
+            />
+            {errors.description && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.description.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="priority"
+              className="block text-sm font-medium mb-1"
+            >
+              Priority
+            </label>
+            <Controller
+              name="priority"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select Assignee" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="LOW">Low</SelectItem>
+                    <SelectItem value="MEDIUM">Meidum</SelectItem>
+                    <SelectItem value="HIGH">High</SelectItem>
+                    <SelectItem value="URGENT">Urgent</SelectItem>
+                  </SelectContent>
+                </Select>
+              )}
+            />
+          </div>
+          {error && <p className="text-red-500 mt-2">{error.message}</p>}
+          <Button
+            type="submit"
+            disabled={createIssueLoading}
+            className="w-full"
+          >
+            {createIssueLoading ? "Creating..." : "Create Issue"}{" "}
+          </Button>
         </form>
       </DrawerContent>
     </Drawer>
