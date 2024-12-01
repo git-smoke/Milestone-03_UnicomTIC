1aef8b8fd93e75f494ba7152c423baf6fc39be2e
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-02 02:28:39 +0530

    color of the card enum init

--- projix/components/issue-cards.jsx
+++ projix/components/issue-cards.jsx
@@ -1,3 +1,10 @@
+const priorityColor = {
+  LOW: "border-green-600",
+  MEDIUM: "border-yellow-300",
+  HIGH: "border-orange-400",
+  URGENT: "border-red-400",
+};
+
 const IssueCard = () => {
   return <div>IssueCard</div>;
 };
