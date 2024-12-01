f592faf09b9cec61c135cf7f72b727ce37fdce27
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-02 02:54:13 +0530

    added user avatar component

---
+++ projix/components/user-avatar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Avatar } from "./ui/avatar";
+
+const UserAvatar = () => {
+  return <div
+  className="">
+    <Avatar></Avatar>
+  </div>;
+};
+
+export default UserAvatar;
