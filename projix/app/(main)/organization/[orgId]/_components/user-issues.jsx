cae5fab2044f8686ab19285adb5977ad8545ff56
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-02 21:39:58 +0530

    basic tabs trigger configured

--- projix/app/(main)/organization/[orgId]/_components/user-issues.jsx
+++ projix/app/(main)/organization/[orgId]/_components/user-issues.jsx
@@ -1,5 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const UserIssues = ({ userId }) => {
-  return <div>UserIssues</div>;
+  return (
+    <>
+      <h1 className="text-4xl font-bold gradient-title mb-4">My Issues</h1>
+
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          Make changes to your account here
+        </TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+    </>
+  );
 };
 
 export default UserIssues;
