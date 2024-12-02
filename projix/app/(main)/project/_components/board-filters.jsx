bcf6669e53c95a40f91ca908d84f7a436e874d52
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-02 18:02:03 +0530

    input element initialized for search function

--- projix/app/(main)/project/_components/board-filters.jsx
+++ projix/app/(main)/project/_components/board-filters.jsx
@@ -5,7 +5,20 @@ const BoardFilters = ({ issues, onFilterChange }) => {
   const [selectedAssignees, setSelectedAssignees] = useState([]);
   cosnt[(selectedPriority, setSelectedPriority)] = useState("");
 
-  return <div>BoardFilters</div>;
+  return (
+    <div>
+      <div className="flex flex-col pr-2 sm:flex-row gap-4 sm:gap-6 mt-6">
+        <Input
+          className="w-full sm:w-72"
+          placeholder="Search Issues..."
+          value={searchTerm}
+          onChange={
+            (e) => setSearchTerm(e.target.value)
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default BoardFilters;
