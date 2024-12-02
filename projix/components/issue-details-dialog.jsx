d4abdb07fc8a68e0a2e8fa08df086b7029d76654
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-02 16:29:11 +0530

    updated styles and logics for issue details card

--- projix/components/issue-details-dialog.jsx
+++ projix/components/issue-details-dialog.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
-const IssueDetailsDialog = () => {
+const IssueDetailsDialog = ({
+  isOpen,
+  onClose,
+  issue,
+  onDelete = () => {},
+  onUpdate = () => {},
+  borderCol = "",
+}) => {
   return (
-    <div>IssueDetailsDialog</div>
-  )
-}
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <div className="flex justify-between items-centers">
+            <DialogTitle className="text-3xl">{issue.title}</DialogTitle>
+          </div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-export default IssueDetailsDialog
+export default IssueDetailsDialog;
